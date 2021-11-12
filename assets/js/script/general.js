77ae296167dc5fce350f828bb36f892a1d831c24
Alex <remstroyod@gmail.com>
2021-11-12 20:21:41 +0200

    Fix

--- assets/js/script/general.js
+++ assets/js/script/general.js
@@ -1,6 +1,6 @@
 /**
  * AllPage
- * @type {{init: AllPage.init}}
+ * @type {{init: Calculator.init}}
  */
 const Calculator = function () {
 
@@ -15,56 +15,14 @@ const Calculator = function () {
      */
     const pluginStart = function () {
 
-        /**
-         * Replace all SVG images with inline SVG
-         */
-		jQuery('img.svg').each(function(){
-			var $img = jQuery(this);
-			var imgID = $img.attr('id');
-			var imgClass = $img.attr('class');
-			var imgURL = $img.attr('src');
-
-			jQuery.get(imgURL, function(data) {
-				// Get the SVG tag, ignore the rest
-				var $svg = jQuery(data).find('svg');
-
-				// Add replaced image ID to the new SVG
-				if(typeof imgID !== 'undefined') {
-					$svg = $svg.attr('id', imgID);
-				}
-				// Add replaced image classes to the new SVG
-				if(typeof imgClass !== 'undefined') {
-					$svg = $svg.attr('class', imgClass+' replaced-svg');
-				}
-
-				// Remove any invalid XML tags as per http://validator.w3.org
-				$svg = $svg.removeAttr('xmlns:a');
-
-				// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-				}
-
-				// Replace image with new SVG
-				$img.replaceWith($svg);
-
-			}, 'xml');
-
-		});
-
 		/**
 		 * Select2
 		 */
 		$('.js-select').select2({
-			placeholder: '- Select a Value -',
-			allowClear: true,
-			minimumResultsForSearch: Infinity
+			placeholder					: '- Select a Value -',
+			allowClear					: true,
+			minimumResultsForSearch		: Infinity,
 		});
-
-
-
-
-
 
 	};
 
@@ -155,7 +113,7 @@ const Calculator = function () {
 
 						$(this).val( val + '%');
 
-						if( key === 37 || key === 39 ) return false;
+						if( key === 37 || key === 39 || key === 9 ) return false;
 
 						/**
 						 * Set Cursor
@@ -190,6 +148,13 @@ const Calculator = function () {
 						 */
 						$this_inp[0].setSelectionRange(0, val_inp.length);
 
+					});
+
+					/**
+					 * Paste
+					 */
+					$inp_visible.on('paste', function(event, parent) {
+						return false;
 					});
 
 				}
@@ -231,7 +196,7 @@ const Calculator = function () {
 
 						$(this).val( '$' + (new Intl.NumberFormat('ja-JP').format(Math.floor(val))));
 
-						if( key === 37 || key === 39 ) return false;
+						if( key === 37 || key === 39 || key === 9 ) return false;
 
 						/**
 						 * Set Cursor
@@ -264,6 +229,13 @@ const Calculator = function () {
 						 */
 						$this_inp[0].setSelectionRange(1, Number(val_inp.length + 1));
 
+					});
+
+					/**
+					 * Paste
+					 */
+					$inp_visible.on('paste', function(event, parent) {
+						return false;
 					});
 
 				}
@@ -375,7 +347,9 @@ const Calculator = function () {
 
 			$(this).val( '$' + (new Intl.NumberFormat('ja-JP').format(Math.floor(val))));
 
-			if( key === 37 || key === 39 ) return false;
+			console.log(key);
+
+			if( key === 37 || key === 39 || key === 9 ) return false;
 
 			/**
 			 * Set Cursor
@@ -399,6 +373,15 @@ const Calculator = function () {
 			 * Set Cursor
 			 */
 			$this_inp[0].setSelectionRange(1, Number(val_inp.length + 1));
+
+		});
+
+		/**
+		 * Paste
+		 */
+		$(document).on('paste', '.inputMoney', function (event, parent) {
+
+			return false;
 
 		});
 
